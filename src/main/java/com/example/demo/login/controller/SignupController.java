d84d93aa52dc126397a2f1dd3178294bc4380ad9
ishida <h_hand_3636@gmail.com>
2020-04-16 16:55:22 +0900

    ErrorMessage

--- src/main/java/com/example/demo/login/controller/SignupController.java
+++ src/main/java/com/example/demo/login/controller/SignupController.java
@@ -5,8 +5,13 @@ import java.util.Map;
 import java.util.LinkedHashMap;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import com.example.demo.domain.model.SignupForm;
 
 @Controller
 public class SignupController {
@@ -23,17 +28,32 @@ public class SignupController {
 		return radio;
 	}
 	
+	//ModelAttributeを引数にセットすることで、自動でモデルに登録をする
+	//クラス名の名前の先頭を小文字にしたものをデフォルトでキーとして登録する
 	@GetMapping("/signup")
-	public String getSignup(Model model) {
+	public String getSignup(@ModelAttribute SignupForm form,Model model) {
 		radioMarriage = initRadioMarriage();
 		
 		model.addAttribute("radioMarriage", radioMarriage);
 		
+		//SignupFormは自動でセットされる
+		
 		return "/login/signup";
 	}
 	
+	//データバインドの結果を受け取るためにBindingResultを引数に設定
+	//@Validatedでバリデーション実施
 	@PostMapping("/signup")
-	public String postSignup(Model model) {
+	public String postSignup(@ModelAttribute @Validated SignupForm form,BindingResult bindingResult, Model model) {
+		
+		//入力チェックに引っかかっていた場合、元のページに戻る
+		if(bindingResult.hasErrors()) {
+			System.out.println("validate error cause in login page");
+			return getSignup(form,model);
+		}
+		
+		System.out.println("validate ok in login page " + form);
+		
 		//リダイレクト
 		return "redirect:/login";
 	}
